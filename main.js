4c7900d3c52dd8429e028773b43c5bc7139cdef3
DavydSt <145857522+DavydSt@users.noreply.github.com>
2025-01-17 17:53:39 +0100

    finish

---
+++ main.js
@@ -0,0 +1,28 @@
+"use strict"
+
+const age = 14;
+console.log(age);
+
+const name = "Davyd";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "fleicht ja";
+console.log(myString);
+
+let myNumber = 2;
+console.log(myNumber + 10);
+
+const myNull = null;
+console.log(myNull);
+
+const userName = prompt("Введіть своє ім'я");
+console.log(typeof userName);
+alert("дарова :)");
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+
+alert("Увага! Дія може бути небезпечною.");
+confirm("Ви впевнені, що хочете продовжити?");
